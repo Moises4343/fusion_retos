076a60889dc409a4a4e240b7214259e4eeeb3005
Moises4343 <193243@ids.upchiapas.edu.mx>
2024-11-19 10:25:49 -0600

    add: Mejora del bot, conversación en ingles

--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -3,8 +3,11 @@ import 'dart:io';
 
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_tts/flutter_tts.dart';
 import 'package:google_generative_ai/google_generative_ai.dart';
 import 'package:shared_preferences/shared_preferences.dart';
+import 'package:speech_to_text/speech_recognition_result.dart';
+import 'package:speech_to_text/speech_to_text.dart';
 
 const apiKey = 'AIzaSyA0-2rXpHK9d9xuE5EOZ3nPgLuEdCKwf70';
 
@@ -22,6 +25,14 @@ class _ChatScreenState extends State<ChatScreen> {
   bool _isConnected = false;
   bool _isBotTyping = false;
 
+  late SpeechToText _speech;
+  bool _isListening = false;
+  late FlutterTts _flutterTts;
+
+  final ScrollController _scrollController = ScrollController();
+
+  bool _isTextNotEmpty = false;
+
   @override
   void initState() {
     super.initState();
@@ -31,6 +42,17 @@ class _ChatScreenState extends State<ChatScreen> {
     );
     _loadMessages();
     _monitorConnectivity();
+
+    _speech = SpeechToText();
+    _flutterTts = FlutterTts();
+    _initializeTts();
+
+    _controller.addListener(_onTextChanged);
+  }
+
+  void _initializeTts() async {
+    await _flutterTts.setLanguage("en-US");
+    await _flutterTts.setPitch(1.0);
   }
 
   Future<void> _monitorConnectivity() async {
@@ -87,27 +109,26 @@ class _ChatScreenState extends State<ChatScreen> {
 
     setState(() {
       _isBotTyping = true;
-      _messages.add({'role': 'bot', 'content': '...'});
+      _isTextNotEmpty = false;
     });
 
     try {
-      // Preparar contexto con los últimos 5 mensajes
       final context = _getLimitedContext(5);
 
-      // Enviar el contexto a la API
       final response = await _model.generateContent([Content.text(context)]);
-      final botResponse = response.text ?? 'Lo siento, no entendí eso.';
+      final botResponse =
+          response.text ?? "I'm sorry, I didn't understand that.";
 
       setState(() {
         _isBotTyping = false;
-        _messages.removeLast();
         _addMessage('bot', botResponse);
       });
+
+      await _flutterTts.speak(botResponse);
     } catch (e) {
       setState(() {
         _isBotTyping = false;
-        _messages.removeLast();
-        _addMessage('bot', 'Hubo un error. Intenta nuevamente.');
+        _addMessage('bot', 'There was an error. Please try again.');
       });
       print('Error: $e');
     }
@@ -115,7 +136,6 @@ class _ChatScreenState extends State<ChatScreen> {
     await _saveMessages();
   }
 
-  // Método para obtener un subconjunto limitado del contexto
   String _getLimitedContext(int limit) {
     final start = _messages.length > limit ? _messages.length - limit : 0;
     final recentMessages = _messages.sublist(start);
@@ -129,32 +149,119 @@ class _ChatScreenState extends State<ChatScreen> {
     setState(() {
       _messages.add({'role': role, 'content': content});
     });
+
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      if (_scrollController.hasClients) {
+        _scrollController.animateTo(
+          _scrollController.position.maxScrollExtent,
+          duration: const Duration(milliseconds: 300),
+          curve: Curves.easeOut,
+        );
+      }
+    });
   }
 
   Widget _buildMessage(Map<String, String> message) {
     final isUser = message['role'] == 'user';
-    return Align(
-      alignment: isUser ? Alignment.centerRight : Alignment.centerLeft,
-      child: Container(
-        margin: const EdgeInsets.symmetric(vertical: 5, horizontal: 10),
-        padding: const EdgeInsets.all(10),
-        decoration: BoxDecoration(
-          color: isUser ? Colors.blue[200] : Colors.grey[300],
-          borderRadius: BorderRadius.circular(10),
+    return Row(
+      mainAxisAlignment:
+          isUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        if (!isUser)
+          const CircleAvatar(
+            child: Icon(Icons.android),
+          ),
+        if (!isUser) const SizedBox(width: 8),
+        Flexible(
+          child: Container(
+            margin: const EdgeInsets.symmetric(vertical: 5),
+            padding: const EdgeInsets.all(10),
+            decoration: BoxDecoration(
+              color: isUser ? Colors.blue[200] : Colors.grey[300],
+              borderRadius: BorderRadius.circular(10),
+            ),
+            child: Text(message['content'] ?? ''),
+          ),
         ),
-        child: Text(message['content'] ?? ''),
-      ),
+        if (isUser) const SizedBox(width: 8),
+        if (isUser)
+          const CircleAvatar(
+            child: Icon(Icons.person),
+          ),
+      ],
     );
+  }
+
+  void _startListening() async {
+    if (!_isListening) {
+      bool available = await _speech.initialize(
+        onStatus: (status) {
+          if (status == 'done') {
+            setState(() {
+              _isListening = false;
+            });
+          }
+        },
+        onError: (error) {
+          print('Error: $error');
+          setState(() {
+            _isListening = false;
+          });
+        },
+      );
+      if (available) {
+        setState(() => _isListening = true);
+        _speech.listen(
+          onResult: _onSpeechResult,
+          localeId: 'en_US',
+        );
+      } else {
+        print('El usuario ha denegado el uso del reconocimiento de voz.');
+      }
+    } else {
+      setState(() => _isListening = false);
+      _speech.stop();
+    }
+  }
+
+  void _onSpeechResult(SpeechRecognitionResult result) {
+    setState(() {
+      _controller.text = result.recognizedWords;
+    });
+  }
+
+  void _onSendPressed() {
+    final message = _controller.text.trim();
+    if (message.isNotEmpty) {
+      _sendMessage(message);
+    }
+  }
+
+  void _onTextChanged() {
+    setState(() {
+      _isTextNotEmpty = _controller.text.trim().isNotEmpty;
+    });
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose(); // Limpia el controller
+    _speech.stop();
+    _flutterTts.stop();
+    _scrollController.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Chat con Gemini')),
+      appBar: AppBar(title: const Text('Chatbot with Gemini')),
       body: Column(
         children: [
           Expanded(
             child: ListView.builder(
+              controller: _scrollController,
               itemCount: _messages.length,
               itemBuilder: (context, index) {
                 return _buildMessage(_messages[index]);
@@ -164,12 +271,15 @@ class _ChatScreenState extends State<ChatScreen> {
           if (_isBotTyping)
             const Padding(
               padding: EdgeInsets.all(8.0),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: Text(
-                  'El bot está escribiendo...',
-                  style: TextStyle(color: Colors.grey),
-                ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  CircleAvatar(
+                    child: Icon(Icons.android),
+                  ),
+                  SizedBox(width: 8),
+                  CircularProgressIndicator(),
+                ],
               ),
             ),
           Padding(
@@ -188,8 +298,8 @@ class _ChatScreenState extends State<ChatScreen> {
                       enabled: _isConnected,
                       decoration: InputDecoration(
                         hintText: _isConnected
-                            ? 'Escribe un mensaje...'
-                            : 'Sin conexión',
+                            ? 'Type a message...'
+                            : 'No connection',
                         border: InputBorder.none,
                         contentPadding: const EdgeInsets.symmetric(
                           horizontal: 10,
@@ -199,18 +309,18 @@ class _ChatScreenState extends State<ChatScreen> {
                     ),
                   ),
                 ),
+                IconButton(
+                  icon: Icon(_isListening ? Icons.mic : Icons.mic_off),
+                  onPressed: _isConnected ? _startListening : null,
+                ),
                 const SizedBox(width: 8),
                 IconButton(
                   icon: const Icon(Icons.send),
-                  color: _isConnected ? Colors.blue : Colors.grey,
-                  onPressed: _isConnected
-                      ? () {
-                          final message = _controller.text.trim();
-                          if (message.isNotEmpty) {
-                            _sendMessage(message);
-                          }
-                        }
-                      : null,
+                  color: (_isConnected && _isTextNotEmpty)
+                      ? Colors.blue
+                      : Colors.grey,
+                  onPressed:
+                      (_isConnected && _isTextNotEmpty) ? _onSendPressed : null,
                 ),
               ],
             ),
